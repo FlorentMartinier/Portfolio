62f6f73657be43e782276daa0d71e258f9954e94
Florent Martinier <florent.martinier@free.fr>
2026-04-23 21:36:56 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,65 @@
+import { NgForOf } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Project {
+  title: string;
+  tag: string;
+  image: string;
+  description: string;
+  link: string;
+  transform: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [NgForOf]
 })
 export class AppComponent {
-  title = 'portfolio';
+  projects: Project[] = [
+    {
+      title: 'Barrel Manager',
+      tag: 'Android / Mobile',
+      image: 'assets/barrel_manager.webp',
+      description: 'Une application spécialisée pour la gestion et classification de barils.',
+      link: 'https://play.google.com/store/apps/details?id=com.fmartinier.barrelclassifier&hl=fr',
+      transform: ''
+    },
+    {
+      title: 'Carte de Pizzas',
+      tag: 'Web / App',
+      image: 'assets/logo_pizza.png',
+      description: 'Interface interactive présentant une sélection gourmande de pizzas.',
+      link: 'https://pizzas-de-florent.netlify.app/',
+      transform: ''
+    },
+    {
+      title: 'Killer Party',
+      tag: 'Game / Social',
+      image: 'assets/killer_party.webp',
+      description: 'L\'application compagnon idéale pour organiser vos soirées Killer.',
+      link: 'https://killer-party.en.aptoide.com/app',
+      transform: ''
+    }
+  ];
+
+  handleMove(event: MouseEvent, project: Project) {
+    const card = event.currentTarget as HTMLElement;
+    const box = card.getBoundingClientRect();
+    const x = event.clientX - box.left;
+    const y = event.clientY - box.top;
+    const centerX = box.width / 2;
+    const centerY = box.height / 2;
+    
+    // Calcul de l'inclinaison (10 degrés max)
+    const rotateX = (centerY - y) / 10;
+    const rotateY = (x - centerX) / 10;
+
+    project.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1.05, 1.05, 1.05)`;
+  }
+
+  resetCard(project: Project) {
+    project.transform = 'perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)';
+  }
 }
